b37e2251a6324fa6b436908205f4eb56b7d8ca7f
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2021-01-25 21:19:13 -0500

    Refactored interpreter.
    The interpreter can now access the parse tree. As of this writing, it is an arithmetic calculator.

--- tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpeter interpreter = new Interpeter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -29,6 +31,7 @@ public class Lox {
 
         // Indicate an error in the exit code.
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     /* Allows the interpreter to run interactively. */
@@ -54,7 +57,7 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
