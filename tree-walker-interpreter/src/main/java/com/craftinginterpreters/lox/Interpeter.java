86b2293928a17a997571edbe7cda1060df2a6081
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2021-01-25 20:22:46 -0500

    Runtime errors are now recognised and thrown.

--- tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Interpeter.java
+++ tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Interpeter.java
@@ -16,11 +16,25 @@ public class Interpeter implements Expr.Visitor<Object> {
             case BANG:
                 return isTruthy(right);
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
         }
 
         // Unreachable.
         return null;
+    }
+
+    /* Checks if the operator is being applied to a number. */
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator,
+                                     Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
     /*
@@ -60,14 +74,19 @@ public class Interpeter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double)right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double)right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double)right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double)right;
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left - (double)right;
             case PLUS:
                 // Implements addition.
@@ -79,10 +98,13 @@ public class Interpeter implements Expr.Visitor<Object> {
                 if (left instanceof String && right instanceof String) {
                     return (String)left + (String)right;
                 }
-                break;
+                throw new RuntimeError(expr.operator,
+                    "Operands must be two numbers or two strings.");
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left / (double)right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left * (double)right;
             case BANG_EQUAL: return !isEqual(left, right);
             case EQUAL_EQUAL: return isEqual(left, right);
