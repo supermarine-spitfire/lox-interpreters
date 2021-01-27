25a02095566e8505b65d400e9a8285de788e31be
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2021-01-26 21:36:54 -0500

    Refactored Environment.
    Environments now nest, allowing for variable scope.

--- tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Environment.java
+++ tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -4,13 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>(); // Stores the bindings.
+
+    /* For a global scope's environment. */
+    Environment() {
+        enclosing = null;
+    }
+
+    /* For a local scope's environment. */
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     /* Returns the variable's value. */
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+
+        // Recursively search for the variable.
+        if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name,
             "Undefined variable '" + name.lexeme + "'.");
@@ -20,6 +34,12 @@ class Environment {
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        // Recursively search for the variable.
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
