23a47a2e90522013d70c8445f24182d56638dc42
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2021-01-28 15:23:04 -0500

    Refactored AstPrinter.
    visitLogicalExpr() requires further definition.

--- tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/AstPrinter.java
+++ tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/AstPrinter.java
@@ -29,6 +29,12 @@ public class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        // TODO: Implement logical expression printing.
+        return null;
+    }
+
+    @Override
     public String visitUnaryExpr(Expr.Unary expr) {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
