466d7808681c3360d10ab5c5674ee29d934d35ba
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2021-01-30 18:48:58 -0600

    The interpreter can now handle function calls.

--- tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>,
@@ -252,5 +253,30 @@ public class Interpreter implements Expr.Visitor<Object>,
 
         // Unreachable.
         return null;
+    }
+
+    /* Evaluates call (i.e. function-like) expressions. */
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,
+                    "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable)callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected " +
+                    function.arity() + " arguments but got " +
+                    arguments.size() + ".");
+        }
+
+        return function.call(this, arguments);
     }
 }
