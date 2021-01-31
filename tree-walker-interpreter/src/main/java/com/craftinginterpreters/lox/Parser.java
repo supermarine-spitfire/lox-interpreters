7090b5e13201ac792e069692ffc883ffc7f30a0e
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2021-01-30 19:12:27 -0600

    Refactored parser.
    It can now recognise function declarations.

--- tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -40,10 +40,13 @@ public class Parser {
 
     /*
      * Equivalent to the production:
-     * declaration -> varDecl | statement ;
+     * declaration -> funDecl
+     *              | varDecl
+     *              | statement ;
      */
     private Stmt declaration() {
         try {
+            if (match(FUN)) return function("function");
             if (match(VAR)) return varDeclaration();
 
             return statement();
@@ -192,6 +195,31 @@ public class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    /*
+     * Equivalent to the production:
+     * function -> IDENTIFIER "(" parameters? ")" block ;
+     */
+    private Stmt.Function function(String kind) {
+        Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
+        consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
+        List<Token> parameters = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                if (parameters.size() >= 255) {
+                    error(peek(), "Can't have more than 255 parameters.");
+                }
+
+                parameters.add(
+                        consume(IDENTIFIER, "Expect parameter name."));
+            } while (match(COMMA));
+        }
+        consume(RIGHT_PAREN, "Expect ')' after parameters.");
+
+        consume(LEFT_BRACE, "Expect '{' before " + kind + " body.");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
     }
 
     /*
