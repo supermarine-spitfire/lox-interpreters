0a0f18c311aefadda51e98d746bd173413187ed4
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2021-02-03 03:30:41 -0500

    The scanner now recognises the `continue` keyword.

--- tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ tree-walker-interpreter/src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -12,23 +12,24 @@ class Scanner {
 
     static {
         keywords = new HashMap<>();
-        keywords.put("and",    AND);
-        keywords.put("class",  CLASS);
-        keywords.put("else",   ELSE);
-        keywords.put("false",  FALSE);
-        keywords.put("for",    FOR);
-        keywords.put("fun",    FUN);
-        keywords.put("if",     IF);
-        keywords.put("nil",    NIL);
-        keywords.put("or",     OR);
-        keywords.put("print",  PRINT);
-        keywords.put("return", RETURN);
-        keywords.put("super",  SUPER);
-        keywords.put("this",   THIS);
-        keywords.put("true",   TRUE);
-        keywords.put("var",    VAR);
-        keywords.put("while",  WHILE);
-        keywords.put("break",  BREAK);
+        keywords.put("and",      AND);
+        keywords.put("class",    CLASS);
+        keywords.put("else",     ELSE);
+        keywords.put("false",    FALSE);
+        keywords.put("for",      FOR);
+        keywords.put("fun",      FUN);
+        keywords.put("if",       IF);
+        keywords.put("nil",      NIL);
+        keywords.put("or",       OR);
+        keywords.put("print",    PRINT);
+        keywords.put("return",   RETURN);
+        keywords.put("super",    SUPER);
+        keywords.put("this",     THIS);
+        keywords.put("true",     TRUE);
+        keywords.put("var",      VAR);
+        keywords.put("while",    WHILE);
+        keywords.put("break",    BREAK);
+        keywords.put("continue", CONTINUE);
     }
 
     private final String source;
